9537313906cccfa884928349327dd56982ffb098
Brisolo32 <94653435+Brisolo32@users.noreply.github.com>
2022-03-22 15:50:35 -0300

    Update aboutembed.js

--- src/commands/aboutembed.js
+++ src/commands/aboutembed.js
@@ -11,7 +11,7 @@ module.exports = {
             .setAuthor('Sobre o bot | About the bot', 'https://cdn.discordapp.com/app-icons/922283721791504426/ba24abb016d5986dc74643ad5bbf6b70.png?size=256')
             .setDescription('EN-US\nBot Creator: Brisolo32#2968\nSome ideas: Sussy_Oreo#6827\nBig Help: StackOverflow\nGdBrowser: GDColon\n\nPT-BR\nCriador do bot: Brisolo32#2968\nAlgumas ideias: Sussy_Oreo#6827\nAjuda pra caralho: StackOverflow\nGdBrowser: GDColon')
             .setThumbnail('https://cdn.discordapp.com/app-icons/922283721791504426/ba24abb016d5986dc74643ad5bbf6b70.png?size=256')
-            .setFooter("Brisolo32 | Copyleft 2021-2022 | v1.8", "https://cdn.discordapp.com/app-icons/922283721791504426/ba24abb016d5986dc74643ad5bbf6b70.png?size=256")
+            .setFooter("Brisolo32 | Copyleft 2021-2022 | v1.9", "https://cdn.discordapp.com/app-icons/922283721791504426/ba24abb016d5986dc74643ad5bbf6b70.png?size=256")
         
             interaction.reply({ embeds: [about] })
 		}
