784f86f1028667e66918eeab8eab1117476c8ebc
Brisolo32 <94653435+Brisolo32@users.noreply.github.com>
2022-04-22 09:57:01 -0300

    forgor i

--- src/commands/aboutembed.js
+++ src/commands/aboutembed.js
@@ -9,7 +9,7 @@ module.exports = {
         const about = new MessageEmbed()
             .setColor('#ffffff')
             .setAuthor({ name: 'Sobre o bot | About the bot', iconURL: 'https://cdn.discordapp.com/app-icons/922283721791504426/ba24abb016d5986dc74643ad5bbf6b70.png?size=256' })
-            .setDescription('EN-US\nBot Creator: Brisolo32#2968\nSome ideas: Sussy_Oreo#6827\nBig Help: StackOverflow\nGdBrowser: GDColon\n\nPT-BR\nCriador do bot: Brisolo32#2968\nAlgumas ideias: Sussy_Oreo#6827\nAjuda pra caralho: StackOverflow\nGdBrowser: GDColon')
+            .setDescription('EN-US\nBot Creator: Brisolo32#2968\nSome ideas: Sussy_Oreo#6827\nBig Help: StackOverflow\nGdBrowser: GDColon\nCapybara images: r/capybara\n\nPT-BR\nCriador do bot: Brisolo32#2968\nAlgumas ideias: Sussy_Oreo#6827\nAjuda pra caralho: StackOverflow\nGdBrowser: GDColon\nImagens de Capivara: r/capybara')
             .setThumbnail('https://cdn.discordapp.com/app-icons/922283721791504426/ba24abb016d5986dc74643ad5bbf6b70.png?size=256')
             .setFooter({ text: 'Brisolo32 | Copyleft 2021-2022 | v2.1', iconURL: 'https://cdn.discordapp.com/app-icons/922283721791504426/ba24abb016d5986dc74643ad5bbf6b70.png?size=256' })
 
