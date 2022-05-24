d93fa37f5f7410d4795f5865143b521d6d4007ac
Brisolo32 <brisolo32@outlook.com>
2022-05-24 18:42:47 -0300

    oops

--- src/test.js
+++
@@ -1,7 +0,0 @@
-const x = new MessageEmbed()
-    .setColor('#97ff63')
-    .setAuthor('RandWiki | Wikihow', 'https://upload.wikimedia.org/wikipedia/commons/f/fb/WikiHow_initials.png')
-    .setTitle("")
-    .setDescription("Sinopse:\n\n")
-    .setURL('')
-    .setFooter('Providenciado por pt.wikihow.com')
