418be397e7576ffd1a8e7ce64ef5a30c866e91a9
Brisolo32 <brisolo32@outlook.com>
2022-05-25 13:33:42 -0300

    Fixed a lot of shit

--- src/commands/updatembed.js
+++ src/commands/updatembed.js
@@ -12,7 +12,6 @@ module.exports = {
             .setAuthor({ name: 'Updates', iconURL: 'https://cdn.discordapp.com/app-icons/922283721791504426/ba24abb016d5986dc74643ad5bbf6b70.png?size=256' })
             .setDescription(`**Patch-Notes EN-US**\n\n- /music play: plays the video/music from yt\n- /music queue: shows the queue\n- /music skip: skips the music\n- /music remove: removes a music/video from the queue\n- /music pause: pauses the music\n- /music resume: resumes the music\n- /music stop: stops the music\n- /pokesearch: sends a random pokemon or searches a pokemon\n- /poll: added the possibility to add more poll options\n- /gdbrowser leaderboard: shows the ACCURATE gd leaderboard\n- /gdbrowser icon: shows the account icon\n- /capybara: added more capybara images\n- /text: added a stupid \"japanese\" translator`)
             .setFooter({ text: 'Brisolo32 | Copyleft 2021-2022 | v2.1', iconURL: 'https://cdn.discordapp.com/app-icons/922283721791504426/ba24abb016d5986dc74643ad5bbf6b70.png?size=256' })
-
         interaction.reply({ embeds: [updatembed] })
 
 		}
