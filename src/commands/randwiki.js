39c23e1e4c4247e51bdc560306408ee90b0e7ae4
Davi Santos <94653435+Brisolo32@users.noreply.github.com>
2022-05-29 16:43:56 -0300

    Update randwiki.js

--- src/commands/randwiki.js
+++ src/commands/randwiki.js
@@ -1,7 +1,32 @@
+// Shortened the code by a lot
 const { MessageEmbed } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+async function getWiki(uri) {
+  const wikiRnd = await fetch(`${uri}query&list=random&rnnamespace=0&rnlimit=1`)
+  let wkJSON = await wikiRnd.json();
+
+  // Get the title and id of the wiki
+  let wkTitle = wkJSON.query.random[0].title.charAt(0).toUpperCase() + wkJSON.query.random[0].title.slice(1);
+  let wkId = wkJSON.query.random[0].id;  
+
+  // Fetch the full page as HTML
+  const wikiHTML = await fetch(`${uri}parse&prop=text&mobileformat&pageid=${wkId}`);
+  let wiki = await wikiHTML.json();
+  const wikiText = wiki.parse.text['*'];
+  let w = wikiText.toString();
+
+  // Remove the <p> tags, and the <sup> tags with everything inside
+  let wikiP = w.replace(/\<(.*?)\>|\[\d\]/g, ''); // Starting to understand regex
+  
+  // Return the title and the page
+  return {
+    title: wkTitle,
+    page: wikiP
+  }
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -18,116 +43,68 @@ module.exports = {
                 .setRequired(true)),
 	  async execute (interaction) {
         const lang = interaction.options.getString('lang');
-
-        // Welcome to Boilerplate land! Population: me
         if (lang === 'pt') {
-          // Fetch a random wiki from WikiHow's API and also the full page (as HTML)
+          // This will repeat a lot, got it?
+          // Define the wikihow uri for the language
           const ptUri = 'https://pt.wikihow.com/api.php?format=json&action=';
-  
-          const wikiRnd = await fetch(`${ptUri}query&list=random&rnnamespace=0&rnlimit=1`)
-          let wkJSON = await wikiRnd.json();
 
-          // Get the title and id of the wiki
-          let wkTitle = wkJSON.query.random[0].title;
-          let wkId = wkJSON.query.random[0].id;  
-
-          // Fetch the full page as HTML
-          const wikiFull = await fetch(`${ptUri}parse&prop=text&mobileformat&pageid=${wkId}`);
-          let w = await wikiFull.json();
-          const wh = w.parse.text['*'];
-          let wikiHtml = wh.toString();
-          let wikiP = wikiHtml.split('<p>')[1].split('</p>')[0];
+          // Get the wiki
+          const wiki = await getWiki(ptUri);
 
           // Send the wiki's embed
           const wikiEmbed = new MessageEmbed()
             .setColor('#97ff63')
             .setAuthor({ name: 'RandWiki PT', iconURL: 'https://upload.wikimedia.org/wikipedia/commons/f/fb/WikiHow_initials.png' })
-            .setTitle(`${wkTitle}`)
-            .setDescription(`Sinopse:\n${wikiP}`)
+            .setTitle(`${wiki.title}`)
+            .setDescription(`Sinopse:\n${wiki.page}`)
             .setFooter({ text: 'Providenciado por pt.wikihow.com', iconURL: 'https://upload.wikimedia.org/wikipedia/commons/f/fb/WikiHow_initials.png' })         
 
           interaction.reply({ embeds: [wikiEmbed] });
         } else if (lang === 'en') {
           // Fetch a random wiki from WikiHow's API and also the full page (as HTML)
           const enUri = 'https://www.wikihow.com/api.php?format=json&action=';
-  
-          const wikiRnd = await fetch(`${enUri}query&list=random&rnnamespace=0&rnlimit=1`)
-          let wkJSON = await wikiRnd.json();
 
-          // Get the title and id of the wiki
-          let wkT = wkJSON.query.random[0].title;
-          let wkTitle = wkT.charAt(0).toUpperCase() + wkT.slice(1);
-          let wkId = wkJSON.query.random[0].id;  
-
-          // Fetch the full page as HTML
-          const wikiFull = await fetch(`${enUri}parse&prop=text&mobileformat&pageid=${wkId}`);
-          let w = await wikiFull.json();
-          const wh = w.parse.text['*'];
-          let wikiHtml = wh.toString();
-          let wikiP = wikiHtml.split('<p>')[1].split('</p>')[0];
+          // Get the wiki
+          const wiki = await getWiki(enUri);
 
           // Send the wiki's embed
           const wikiEmbed = new MessageEmbed()
             .setColor('#97ff63')
             .setAuthor({ name: 'RandWiki EN', iconURL: 'https://upload.wikimedia.org/wikipedia/commons/f/fb/WikiHow_initials.png' })
-            .setTitle(`${wkTitle}`)
-            .setDescription(`Synopsis:\n${wikiP}`)
+            .setTitle(`${wiki.title}`)
+            .setDescription(`Synopsis:\n${wiki.page}`)
             .setFooter({ text: 'Powerered by www.wikihow.com', iconURL: 'https://upload.wikimedia.org/wikipedia/commons/f/fb/WikiHow_initials.png' })         
 
           interaction.reply({ embeds: [wikiEmbed] });
         } else if (lang === 'es') {
           // Fetch a random wiki from WikiHow's API and also the full page (as HTML)
           const esUri = 'https://es.wikihow.com/api.php?format=json&action=';
-  
-          const wikiRnd = await fetch(`${esUri}query&list=random&rnnamespace=0&rnlimit=1`)
-          let wkJSON = await wikiRnd.json();
 
-          // Get the title and id of the wiki
-          let wkT = wkJSON.query.random[0].title;
-          let wkTitle = wkT.charAt(0).toUpperCase() + wkT.slice(1);
-          let wkId = wkJSON.query.random[0].id;  
-
-          // Fetch the full page as HTML
-          const wikiFull = await fetch(`${esUri}parse&prop=text&mobileformat&pageid=${wkId}`);
-          let w = await wikiFull.json();
-          const wh = w.parse.text['*'];
-          let wikiHtml = wh.toString();
-          let wikiP = wikiHtml.split('<p>')[1].split('</p>')[0];
+          // Get the wiki
+          const wiki = await getWiki(esUri);
 
           // Send the wiki's embed
           const wikiEmbed = new MessageEmbed()
             .setColor('#97ff63')
             .setAuthor({ name: 'RandWiki ES', iconURL: 'https://upload.wikimedia.org/wikipedia/commons/f/fb/WikiHow_initials.png' })
-            .setTitle(`${wkTitle}`)
-            .setDescription(`Sinopsis:\n${wikiP}`)
+            .setTitle(`${wiki.title}`)
+            .setDescription(`Sinopsis:\n${wiki.page}`)
             .setFooter({ text: 'Proporcionado por es.wikihow.com', iconURL: 'https://upload.wikimedia.org/wikipedia/commons/f/fb/WikiHow_initials.png' })         
 
           interaction.reply({ embeds: [wikiEmbed] });
         } else if (lang === 'ru') {
           // Fetch a random wiki from WikiHow's API and also the full page (as HTML)
           const ruUri = 'https://ru.wikihow.com/api.php?format=json&action=';
-  
-          const wikiRnd = await fetch(`${ruUri}query&list=random&rnnamespace=0&rnlimit=1`)
-          let wkJSON = await wikiRnd.json();
 
-          // Get the title and id of the wiki
-          let wkT = wkJSON.query.random[0].title;
-          let wkTitle = wkT.charAt(0).toUpperCase() + wkT.slice(1);
-          let wkId = wkJSON.query.random[0].id;
-
-          // Fetch the full page as HTML
-          const wikiFull = await fetch(`${ruUri}parse&prop=text&mobileformat&pageid=${wkId}`);
-          let w = await wikiFull.json();
-          const wh = w.parse.text['*'];
-          let wikiHtml = wh.toString();
-          let wikiP = wikiHtml.split('<p>')[1].split('</p>')[0];
+          // Get the wiki
+          const wiki = await getWiki(ruUri);
 
           // Send the wiki's embed
           const wikiEmbed = new MessageEmbed()
             .setColor('#97ff63')
             .setAuthor({ name: 'RandWiki РУ', iconURL: 'https://upload.wikimedia.org/wikipedia/commons/f/fb/WikiHow_initials.png' })
-            .setTitle(`${wkTitle}`)
-            .setDescription(`Синопсис:\n${wikiP}`)
+            .setTitle(`${wiki.title}`)
+            .setDescription(`Синопсис:\n${wiki.page}`)
             .setFooter({ text: 'Предоставлено ru.wikihow.com', iconURL: 'https://upload.wikimedia.org/wikipedia/commons/f/fb/WikiHow_initials.png' })         
 
           interaction.reply({ embeds: [wikiEmbed] });
